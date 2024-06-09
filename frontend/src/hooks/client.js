cca90cc56f0909f3cbad4cd325c171d3399eb74c
8Pmjadhav <2020bcs151@sggs.ac.in>
2024-06-09 18:46:07 +0530

    docker_url

--- frontend/src/hooks/client.js
+++ frontend/src/hooks/client.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const client = axios.create({baseURL :`${import.meta.env.VITE_PRODUCTION === 'remote' ? 'https://project-iunl.onrender.com': (import.meta.env.VITE_PRODUCTION === 'docker' ? 'http://backend_c:3000' : 'http://localhost:3000')}`});
+const client = axios.create({baseURL :`${import.meta.env.VITE_PRODUCTION === 'remote' ? import.meta.env.VITE_BACKEND_SERVER: (import.meta.env.VITE_PRODUCTION === 'docker' ? import.meta.env.VITE_BACKEND_SERVER_DOCKER : 'http://localhost:3000')}`});
 
 export default client;
