9d0fd417f0a1a15c4a07a1dd18e2913e194dd92d
8Pmjadhav <2020bcs151@sggs.ac.in>
2024-06-09 19:37:05 +0530

    error1

--- frontend/src/hooks/client.js
+++ frontend/src/hooks/client.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 const client = axios.create({baseURL :`${import.meta.env.VITE_PRODUCTION === 'remote' ? import.meta.env.VITE_BACKEND_SERVER: (import.meta.env.VITE_PRODUCTION === 'docker' ? import.meta.env.VITE_BACKEND_SERVER_DOCKER : 'http://localhost:3000')}`});
-
+console.log(import.meta.env.VITE_PRODUCTION,import.meta.env.VITE_BACKEND_SERVER_DOCKER,import.meta.env.VITE_BACKEND_SERVER);
 export default client;
